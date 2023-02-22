8a65001059c6ec29be1129e33270d0cfc7b63e00
tianwens <18825035571@163.com>
2023-02-22 08:38:39 +0800

    1

--- frontend/src/App.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
-
-test('renders full stack', () => {
-  render(<App />);
-  const textElement = screen.getByText(/full-stack/i);
-  expect(textElement).toBeInTheDocument();
-});
